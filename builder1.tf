75e930ad88129e63f502367a967361b5ad1c6b1b
mvolkov82 <leopard@inbox.ru>
2023-04-05 20:55:37 +0200

    Added Jenkins-agent1

--- builder1.tf
+++ builder1.tf
@@ -39,6 +39,10 @@ docker run --rm -e AWS_ACCESS_KEY_ID=${var.key_id} -e AWS_SECRET_ACCESS_KEY=${va
 docker run --rm -e AWS_ACCESS_KEY_ID=${var.key_id} -e AWS_SECRET_ACCESS_KEY=${var.key} -v /opt/java_project:/aws amazon/aws-cli s3 cp s3://malvolkov02/Dockerfile .
 EOF
 }
+//В проде Dockerfile появится, а артифакт нет из-за того, что прод будет создан раньше чем builderPC успеет собрать артифакт.
+// поэтому в проде надо вручную запустить команды export ключей (как в терраформе) а затем: (обрати внимание ключи прописаны не так как в тераформ скрипте)
+//docker run --rm -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -v $(pwd):/aws amazon/aws-cli s3 cp s3://malvolkov02/hello-1.0.war .
+
 
 //resource "aws_instance" "aws_sender" {
 //  ami = "ami-0caef02b518350c8b"
